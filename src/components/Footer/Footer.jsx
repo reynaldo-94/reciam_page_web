630a4804db4caddeeb3ae23e4227060a1bc135da
REYNALDO ADRIAN CAUCHE VILCA <reynaldocauche@gmail.com>
2024-10-14 15:14:43 -0500

    update changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { Email, FacebookOutlined, Instagram, WhatsApp, YouTube } from '@mui/icons-material';
-import { Box, Divider, Grid, Link, TextField, Typography, styled } from '@mui/material';
+import { Box, Divider, Grid, Link, TextField, Typography, styled, useMediaQuery, useTheme } from '@mui/material';
 import { GridInfoDetailFooter, GridInfoLogoFooter, GridInfoSocialFooter } from './constants';
 import { FontSize, ThemePalette } from '../../theme/theme';
 import { CButton } from '../Button';
@@ -15,10 +15,14 @@ const dataFooter = [
 
 export const Footer = () => {
 
+  const theme = useTheme();
+
   const navigate = useNavigate();
 
+  const onlySmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
-    <Box marginTop='20px' p='20px 50px' component="footer" display='flex' flexDirection='column' sx={{ backgroundColor: ThemePalette.SECTION_BACKGROUND_GREEN }} color={ThemePalette.TEXT_WHITE} gap='7px' >
+  <Box marginTop='20px' p='20px 50px' component="footer" display='flex' flexDirection='column' sx={{ backgroundColor: ThemePalette.SECTION_BACKGROUND_GREEN }} color={ThemePalette.TEXT_WHITE} gap='7px' >
     <Box display='flex' justifyContent='space-between' flexWrap='wrap' gap='10px'>
       <Box display='flex' flexDirection='column' gap='8px' flex={1}>
         <Box display='flex' flexDirection='column' gap='2px'>
@@ -47,32 +51,7 @@ export const Footer = () => {
           }
         </Box>
       </Box>
-      
-      {/* <Box display='flex' flexDirection='column' gap='8px' flex={1}>
-        <Box display='flex' flexDirection='column' gap='2px'>
-          <Typography color={ThemePalette.WHITE} fontWeight='bold' >Servicios</Typography>
-          <Divider sx={{
-            bgcolor: ThemePalette.WHITE,
-            width: '100px'
-          }} />
-        </Box>
-        
-        
-        <Box>
-          {
-            services.map((res) => (
-              <Typography color={ThemePalette.WHITE} key={res.id}
-                onClick={() => navigate(res.path)}
-                sx={{
-                  cursor: 'pointer'
-                }}
-              >
-                {res.label}
-              </Typography>
-            ))
-          }
-        </Box>
-      </Box> */}
+
       <Box display='flex' flexDirection='column' gap='10px' flex={1}>
         <Box display='flex' flexDirection='column' gap='6px'>
           
@@ -85,7 +64,7 @@ export const Footer = () => {
           </Box>
 
           <Box>
-            <Typography color={ThemePalette.WHITE}>Marquez de Corpac Chorreado Mz A Lote 1 Chorrillos</Typography>
+            <Typography color={ThemePalette.WHITE}>Marquez de Corpac Mz A Lote 1 Chorrillos</Typography>
           </Box>
         </Box>
         <Box display='flex' flexDirection='column' color={ThemePalette.WHITE}>
@@ -98,18 +77,18 @@ export const Footer = () => {
           </Box>
           <Box display='flex' gap='8px'>
             <Email />
-            <Typography>reaciam@gmail.com</Typography>
+            <Typography>reciamsac20@gmail.com</Typography>
           </Box>
         </Box>
       </Box>
-      <Box display='flex' flexDirection='column' gap='10px' flex={1} alignItems='end'>
+      <Box display='flex' flexDirection='column' gap='10px' flex={1} alignItems={onlySmallScreen ? 'start' : 'end'}>
         <Box display='flex' gap='5px'>
           <WhatsApp fontSize='large' sx={{ color: '#fff' }} />
           <Box display='flex' flexDirection='column' color={ThemePalette.WHITE}>
             <Typography>Escríbenos ahora</Typography>
             <Typography textAlign='center' onClick={() => window.open('https://wa.me/51916502605', '_blank')} sx={{
               cursor: 'pointer'
-            }} >945740524</Typography>
+            }} >931227372</Typography>
           </Box>
         </Box>
         {/* <Box display='flex' justifyContent='center'>
